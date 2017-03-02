0061f255b9002e5fe782c238fa01d1544e444e0c
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-03-02 16:36:54 +0000

    change testphrases to data_char_testphrases

--- R/data.R
+++ R/data.R
@@ -1,13 +1,11 @@
 
-#' @name testphrases
-#' @docType data
-#' @title sample short documents for testing
-#' @description Some sample short documents in plain text format for testing
+#' sample short documents for testing
+#' 
+#' Some sample short documents in plain text format for testing
 #'   with \code{\link{liwcalike}}.
 #' @examples
-#' liwcalike(testphrases)
-#'
-NULL
+#' liwcalike(data_char_testphrases)
+"data_char_testphrases"
 
 # save(testphrases, file = "data/testphrases.RData")
 # writeLines(testphrases, "inst/extdata/testphrases.txt")
