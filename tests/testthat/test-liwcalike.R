f7fbae3847b6fa79cab35d54a05b35f6c6c74cf6
HaiyanLW <wangh1651@gmail.com>
2017-11-06 18:16:12 +0000

    Add more tests

--- tests/testthat/test-liwcalike.R
+++ tests/testthat/test-liwcalike.R
@@ -1,6 +1,6 @@
 context('test liwcalike.R')
 
-test_that("test dictionary count", {
+test_that("test dictionary count etc.", {
     
     txt <- c("The red-shirted lawyer gave her ex-boyfriend $300 out of pity :(.",
              "The green-shirted lawyer gave her $300 out of pity :(.")
@@ -12,6 +12,12 @@ test_that("test dictionary count", {
     
     toks <- tokens(txt[1], remove_hyphens = TRUE)
     num_words_txt1 <- ntoken(toks)
+    
+    # dictionary count
     num_people <- sum(toks$text1 == "lawyer") + sum(toks$text1 == "boyfriend")
     expect_equivalent(round(as.numeric(myCount$people[1]), 2), round(100*num_people/num_words_txt1, 2))
+
+    # period count
+    num_Period <- stringi::stri_count_fixed(txt[1], ".") / num_words_txt1 * 100
+    expect_equivalent(round(as.numeric(myCount$Period[1]), 2), round(num_Period, 2))
 })
